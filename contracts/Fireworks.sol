9a2fa04a660cc9329396854d52832d8f95a7617b
lukema95 <867273263@qq.com>
2023-01-08 22:40:37 +0800

    feat: add erc1155 burn

--- contracts/Fireworks.sol
+++ contracts/Fireworks.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-
-contract Fireworks is IERC165, IERC721Receiver {
+contract Fireworks is IERC165, IERC721Receiver, IERC1155Receiver {
 
   using ERC165Checker for address;
 
@@ -20,43 +20,57 @@ contract Fireworks is IERC165, IERC721Receiver {
     return interfaceId == IID_IERC165;
   }
 
-  function isERC1155(address tokenAddress) internal  returns (bool) {
+  function isERC1155(address tokenAddress) internal view returns (bool) {
     return tokenAddress.supportsInterface(IID_IERC1155);
   }    
     
-  function isERC721(address tokenAddress) internal returns (bool) {
+  function isERC721(address tokenAddress) internal view returns (bool) {
     return tokenAddress.supportsInterface(IID_IERC721);
   }
 
   /**
-     * Always returns `IERC721Receiver.onERC721Received.selector`.
-     */
-    function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
-        return this.onERC721Received.selector;
-    }
+  * Always returns `IERC721Receiver.onERC721Received.selector`.
+  */
+  function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
+    return this.onERC721Received.selector;
+  }
+
+  function onERC1155Received(address, address, uint256, uint256, bytes memory) public virtual returns (bytes4) {
+    return this.onERC1155Received.selector;
+  }
+
+  function onERC1155BatchReceived(
+    address,
+    address,
+    uint256[] calldata,
+    uint256[] calldata,
+    bytes calldata
+  ) public virtual override returns (bytes4)
+  {
+    return this.onERC1155BatchReceived.selector;
+  }
 
   /**
    * @dev 
    */
-  function burn(address tokenContract, uint256[] calldata tokenIds, uint256[] calldata amount) external {
+  function burn(address tokenContract, uint256[] calldata tokenIds, uint256[] calldata amounts) external {
     require(_checkContract(tokenContract), "contract address error");
     require(_verifyApprove(tokenContract), "not authorized by contract");
-    _transferToken(tokenContract, msg.sender, tokenIds, amount);
+    _transferToken(tokenContract, msg.sender, tokenIds, amounts);
   }
-
   
-  function _transferToken(address tokenContract, address from, uint256[] calldata tokenIds, uint256[] calldata amount) internal {
-    if (isERC721(tokenContract)) {
-      for(uint256 i = 0; i < tokenIds.length; i++) {
-        IERC721(tokenContract).safeTransferFrom(msg.sender, address(this), tokenIds[i]);
+  function _transferToken(address tokenContract, address from, uint256[] calldata tokenIds, uint256[] calldata amounts) internal {
+      if (isERC721(tokenContract)) {
+        for(uint256 i = 0; i < tokenIds.length; i++) {
+          IERC721(tokenContract).safeTransferFrom(msg.sender, address(this), tokenIds[i]);
+        }
+      }else {
+        IERC1155(tokenContract).safeBatchTransferFrom(from, address(this), tokenIds, amounts, "");
       }
-      
-    }else {
-      // todo
-    }
   }
 
-  function _verifyApprove(address tokenContract) internal returns(bool) {
+  // verify that the token contract is authorized to the contract
+  function _verifyApprove(address tokenContract) internal view returns(bool) {
     if (isERC721(tokenContract)) {
       return IERC721(tokenContract).isApprovedForAll(msg.sender, address(this));
     }else {
@@ -66,7 +80,7 @@ contract Fireworks is IERC165, IERC721Receiver {
   }
 
   // check whether the given contract is ERC721 or ERC1155, and return false if not
-  function _checkContract(address tokenAddress) internal returns(bool) {
+  function _checkContract(address tokenAddress) internal view returns(bool) {
     return isERC1155(tokenAddress) || isERC721(tokenAddress);
   }
   
